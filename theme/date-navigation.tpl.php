a0457f4401384634f079a734301e77894c8f1051
Karen Stevenson <karens@45874.no-reply.drupal.org>
2008-06-10 16:01:08 +0000

    More work to get the generic date argument handler pulled completely out of the Calendar module and into the Date API in a way that it can be used for any Views date field in any view.

--- theme/date-navigation.tpl.php
+++ theme/date-navigation.tpl.php
@@ -5,19 +5,18 @@
  * Template to display date navigation links.
  *
  * $nav_title
- *   The formatted title for this calendar. In the case of mini
- *   calendars, it will be a link to the full view of the calendar,
- *   otherwise it will be the formatted name of the year, month, day,
- *   or week.
+ *   The formatted title for this view. In the case of block
+ *   views, it will be a link to the full view, otherwise it will 
+ *   be the formatted name of the year, month, day, or week.
  * $prev_url
  * $next_url
  *   Urls for the previous and next calendar pages. The links are 
  *   composed in the template to make it easier to change the text,
  *   add images, etc.
- * $mini: Whether or not this is a mini calendar.
- * $block: Whether or not this calendar is in a block.
+ * $block: 
+ *   Whether or not this view is in a block.
  * $view
- *   The view object for this calendar.
+ *   The view object for this navigation.
  * 
  * The &nbsp; in the prev and next divs is to be sure they are never
  * completely empty, needed in some browsers to prop the header open
@@ -28,7 +27,7 @@
 <div class="date-nav clear-block">
   <div class="prev">
     <?php if (!empty($prev_url)) : ?>
-      <span class="next"> <?php print l($mini ? '«' : t('« prev'), $prev_url); ?></span>
+      <span class="next"> <?php print l($block ? '«' : t('« prev'), $prev_url); ?></span>
     <?php endif; ?>
   &nbsp;</div>
   <div class="heading">
@@ -36,7 +35,7 @@
   </div>
   <div class="next">&nbsp;
     <?php if (!empty($next_url)) : ?>
-      <span class="next"> <?php print l($mini ? '»' : t('next »'), $next_url); ?></span>
+      <span class="next"> <?php print l($block ? '»' : t('next »'), $next_url); ?></span>
     <?php endif; ?>  
   </div>
 </div>
