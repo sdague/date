f4ee03f2d0197053dc2957f5f4cb8329eab8f7a7
Karen Stevenson <karens@45874.no-reply.drupal.org>
2008-07-22 11:00:11 +0000

    Move the class configuration to the theme so it is easier for themers.

---
+++ theme/date-views-filter-form.tpl.php
@@ -0,0 +1,11 @@
+<div class="container-inline-date clear-block">
+  <?php print $date ?>
+  <?php print $mindate ?>
+  <?php print $maxdate ?>
+</div>
+<div>
+  <?php print $default_date ?>
+  <?php print $default_to_date ?>
+  <?php print $description ?>
+</div>
+  
