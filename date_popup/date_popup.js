12fa37dfa4d58058b5fb4572d15cf9472c0c32c3
Karen Stevenson <karens@45874.no-reply.drupal.org>
2008-07-26 11:20:31 +0000

    Update to latest versions of jQuery datepicker and timeentry. Add test for jQueryUI and load datepicker from there if it exists.

--- date_popup/date_popup.js
+++ date_popup/date_popup.js
@@ -10,14 +10,14 @@ Drupal.behaviors.date_popup = function (context) {
         var datePopup = e.data;
         // Explicitely filter the methods we accept.
         switch (datePopup.func) {
-          case 'calendar':
+          case 'datepicker':
             if (datePopup.settings.fromTo) {
               // TODO : unsure what the id's for 'From' and 'To' are actually supposed to be
               // datePopup.settings.minDate = (this.id == 'dTo' ? getDate($('#dFrom').val()) : null);
               // datePopup.settings.maxDate = (this.id == 'dFrom' ? getDate($('#dTo').val()) : null);
             }
             $(this)
-              .calendar(datePopup.settings)
+              .datepicker(datePopup.settings)
               .addClass('date-popup-init')
               .focus();
             break;
